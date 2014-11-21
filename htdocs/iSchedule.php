2029e24f1a277bbe33065b53bf3445aaa21c1260
Jim Fenton <fenton@bluepopcorn.net>
2014-11-20 22:33:08 -0800

    Merge branch issue_20_localization into master

--- htdocs/iSchedule.php
+++ htdocs/iSchedule.php
@@ -8,26 +8,37 @@ include("interactive-page.php");
 require_once("AwlQuery.php");
 require_once('iSchedule.php');
 
+
+$heading_schedule = translate('iSchedule Configuration');
+$content_sched1 = translate('iSchedule allows caldav servers to communicate directly with each other, bypassing the need to send invitations via email, for scheduled events where attendees are using different servers or providers. Additionally it enables freebusy lookups for remote attendees. Events and ToDos received via iSchedule will show up in the users scheduling inbox.');
+$content_sched2 = translate('The <a href="http://wiki.davical.org/w/iSchedule_configuration">iSchedule configuration</a> requires a few DNS entries. DNS SRV record(s) will need to be created for all domains you wish to accept requests for, these are the domain portion of email address on Principal records in DAViCal examples are listed below for domains found in your database. At least 1 public key must also be published if you wish to send requests from this server.');
+
+
  $page_elements = array();
  $page_elements[] = <<<EOBODY
-<h1>iSchedule Configuration</h1>
-<p>iSchedule allows caldav servers to communicate directly with each other, bypassing the need to send invitations via email, for scheduled events where attendees are using different servers or providers.  Additionally it enables freebusy lookups for remote attendees.  Events and ToDos received via iSchedule will show up in the users scheduling inbox.
-<br>&nbsp;
-</p>
-<p>The <a href="http://wiki.davical.org/w/iSchedule_configuration">iSchedule configuration</a> requires a few DNS entries. DNS SRV record(s) will need to be created for all domains you wish to accept requests for, these are the domain portion of email address on Principal records in DAViCal examples are listed below for domains found in your database. At least 1 public key must also be published if you wish to send requests from this server.</p>
+<h1>$heading_schedule</h1>
+<p>$content_sched1
+<br>&nbsp;</p>
+<p>$content_sched2</p>
 EOBODY;
 
-$status = '<h2>Status</h2>';
+$translated_error = translate('ERROR');
+$translated_err1 = translate('scheduling_dkim_domain not set (please check your DAViCal configuration file)');
+
+$translated_warning = translate('WARNING');
+$translated_war1 = translate('scheduling_dkim_domain does not match server name (please check your DAViCal configuration file)');
+
+$status = '<h2>' . translate('Status') . '</h2>';
 if (!isset($c->scheduling_dkim_domain)) {
   $status .= <<<EOBODY
-<div class='error'>ERROR</div>
-<p>scheduling_dkim_domain not set</p>
+<div class='error'>$translated_error</div>
+<p>$translated_err1</p>
 EOBODY;
 }
 elseif ( $c->scheduling_dkim_domain != $_SERVER['SERVER_NAME'] ) {
   $status .= <<<EOBODY
-<div class='error'>WARNING</div>
-<p>scheduling_dkim_domain does not match server name</p></h3>
+<div class='error'>$translated_warning</div>
+<p>$translated_war1</p></h3>
 EOBODY;
 }
 else {
@@ -42,30 +53,29 @@ function checkiSchedule () {
   $s = new iSchedule ();
   $s->domain = $c->scheduling_dkim_domain;
   if (!$s->getServer())
-    $ret .= '<p>SRV record missing for '. $s->domain .' or DNS failure, the domain you are going to send events from should have an SRV record</p>' ;
+    $ret .= '<p>' . sprintf(translate('SRV record missing for "%s" or DNS failure, the domain you are going to send events from should have an SRV record'), $s->domain) . '</p>';
   if ($s->remote_server != $c->scheduling_dkim_domain)
-    $ret .= '<p>SRV record for '.$s->domain . ' points to wrong domain: ' . $s->remote_server . ' instead of ' . $c->scheduling_dkim_domain  .'</p>';
+    $ret .= '<p>' . sprintf(translate('SRV record for "%s" points to wrong domain: "%s" instead of "%s"'), $s->domain, $s->remote_server, $c->scheduling_dkim_domain) . '</p>';
   $s->remote_server = $c->scheduling_dkim_domain;
   $s->remote_selector = $c->scheduling_dkim_selector;
   if (!$s->getTxt()) {
     if (isset($c->schedule_private_key))
-      $ret .= '<p>TXT record missing for '.$s->remote_selector . '._domainkey.' . $s->domain .' or DNS failure, Private RSA key is configured</p>' ;
+      $ret .= '<p>' . translate('TXT record missing for "%s._domainkey.%s" or DNS failure, Private RSA key is configured', $s->remote_selector, $s->domain) . '</p>';
     else {
       $keys = generateKeys();
-      $config = '<p>please add the following section to your DAViCal configuration file<pre>$c->schedule_private_key = &lt;&lt;&lt;ENDOFKEY' ."\n";
+      $config = '<p>' . translate('please add the following section to your DAViCal configuration file') . '<pre>$c->schedule_private_key = &lt;&lt;&lt;ENDOFKEY' . "\n";
       $config .= $keys['private']; //implode ("\n", str_split ( base64_encode ( $keys['private'] ), 64 ));
       $config .= "ENDOFKEY\n</pre>";
-      $config .= "<br/> and create a DNS TXT record for <b>" . $c->scheduling_dkim_selector . "._domainkey." . $c->scheduling_dkim_domain ;
-      $config .= "</b> that contains:<pre>";
-      $config .= "k=rsa; t=s; p=" . preg_replace('/-----(BEGIN|END) PUBLIC KEY-----\n/','',$keys['public']);
+      $config .= "<br/> " . sprintf(translate('and create a DNS TXT record for <b>%s._domainkey.%s</b> that contains:'), $c->scheduling_dkim_selector, $c->scheduling_dkim_domain);
+      $config .= "<pre>k=rsa; t=s; p=" . preg_replace('/-----(BEGIN|END) PUBLIC KEY-----\n/','',$keys['public']);
       $config .= '</pre></p>';
       $ret .= $config;
     }
   }
   if ( ! $s->parseTxt() )
-    $ret .= '<p>TXT record corrupt for '. $s->remote_selector .'._domainkey.'. $s->domain .' or DNS failure</p>' ;
+    $ret .= '<p>' . sprintf(translate('TXT record corrupt for %s._domainkey.%s or DNS failure'), $s->remote_selector, $s->domain) . '</p>';
   else if ( $ret == '' )
-    $ret = '<p>iSchedule OK</p>';
+    $ret = '<p>' . translate('iSchedule OK') . '</p>';
   return $ret;
 }
 
