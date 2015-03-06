642524d391298839fba1bdcd86d5ee37daf497e5
Cyril Giraud <cgiraud@free.fr>
2015-03-06 23:41:08 +0100

    Extract translatable strings in upgrade.php + update according to Transifex translations.

--- htdocs/upgrade.php
+++ htdocs/upgrade.php
@@ -6,19 +6,23 @@ $session->LoginRequired('Admin');
 
 include("interactive-page.php");
 
+ $heading_pagelem1 = translate('Upgrade Database');
+ $content_pagelem11 = translate('Currently this page does nothing. Suggestions or patches to make it do something useful will be gratefully received.');
+
+ $heading_pagelem2 = translate('Upgrading DAViCal Versions');
+ $content_pagelem21 = translate('The <a href="http://wiki.davical.org/w/Update-davical-database">update-davical-database</a> should be run manually after upgrading the software to a new version of DAViCal.');
+ $content_pagelem22 = translate("In due course this program will implement the functionality which is currently contained in that script, but until then I'm afraid you do need to run it.");
+
  $page_elements = array();
  $page_elements[] = <<<EOBODY
-<h1>Upgrade Database</h1>
-<p>Currently this page does nothing.  Suggestions or patches to make it do something
-useful will be gratefully received.
+<h1>$heading_pagelem1</h1>
+<p>$content_pagelem11
 <br>&nbsp;
 </p>
-<h2>Upgrading DAViCal Versions</h2>
-<p>The <a href="http://wiki.davical.org/w/Update-davical-database">update-davical-database</a> should be run
-manually after upgrading the software to a new version of DAViCal.</p>
+<h2>$heading_pagelem2</h2>
+<p>$content_pagelem21</p>
 
-<p>In due course this program will implement the functionality which is currently contained in that
-script, but until then I'm afraid you do need to run it.
+<p>$content_pagelem22</p>
 EOBODY;
 
 
@@ -46,16 +50,19 @@ $prompt_app_user = translate('Application DB User');
 $prompt_do_patch = translate('Apply DB Patches');
 $prompt_owner = translate('Database Owner');
 
+$content_template1 = translate('Connection Parameters');
+$content_template2 = translate('Operation Parameters');
+
 $template = <<<EOTEMPLATE
 ##form##
 <table>
- <tr> <th class="h2" colspan="2">Connection Parameters</th> </tr>
+ <tr> <th class="h2" colspan="2">$content_template1</th> </tr>
  <tr> <th class="right">$prompt_dbhost:</th>           <td class="left">##dbhost.input.20##</td> </tr>
  <tr> <th class="right">$prompt_dbport:</th>      <td class="left">##dbport.input.5##</td> </tr>
  <tr> <th class="right">$prompt_dbname:</th>    <td class="left">##dbname.input.20##</td> </tr>
  <tr> <th class="right">$prompt_dbuser:</th>         <td class="left">##dbuser.input.20##</td> </tr>
  <tr> <th class="right">$prompt_dbpass:</th>        <td class="left">##dbpass.password.20##</td> </tr>
- <tr> <th class="h2" colspan="2">Operation Parameters</th> </tr>
+ <tr> <th class="h2" colspan="2">$content_template2</th> </tr>
  <tr> <th class="right">$prompt_app_user:</th>         <td class="left">##app_user.input.20##</td> </tr>
  <tr> <th class="right">$prompt_do_patch:</th>      <td class="left">##apply_patches.checkbox##</td> </tr>
  <tr> <th class="right">$prompt_owner:</th>         <td class="left">##owner.input.20##</td> </tr>
