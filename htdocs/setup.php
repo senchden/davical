fee15797f7a7363163b5ced89ffe6e1e5ccda116
Florian Schlichting <fsfs@debian.org>
2016-06-01 22:40:46 +0200

    Check for PHP XML support in setup.php (see #91)

--- htdocs/setup.php
+++ htdocs/setup.php
@@ -224,6 +224,10 @@ function check_datetime() {
   return $result;
 }
 
+function check_xml() {
+  return new CheckResult(function_exists('xml_parser_create_ns'));
+}
+
 function check_schema_version() {
   global $c;
   if ( $c->want_dbversion[0] == $c->schema_major
@@ -324,6 +328,7 @@ function build_dependencies_table( ) {
     translate('GNU gettext support')              => 'check_gettext',
     translate('PHP iconv support')                => 'check_iconv',
     translate('PHP DateTime class')               => 'check_datetime',
+    translate('PHP XML support')                  => 'check_xml',
     translate('Suhosin "server.strip" disabled')  => 'check_suhosin_server_strip',
     translate('PHP Magic Quotes GPC off')         => 'check_magic_quotes_gpc',
     translate('PHP Magic Quotes runtime off')     => 'check_magic_quotes_runtime',
