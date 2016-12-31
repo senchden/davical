3b85efc88ab702d022fac7258e3053d88a9736f6
Florian Schlichting <fsfs@debian.org>
2016-12-31 17:34:10 +0100

    document AWL debug logging improvements

--- config/debug-config.php
+++ config/debug-config.php
@@ -1,8 +1,11 @@
 <?php
 /**
 * The configuration settings detailed here should probably not appear in most
-* most people's configuration files, but they can be useful to have available
-* to assist with debugging problems.
+* people's configuration files, but they can be useful to assist with debugging
+* problems. When reporting an issue in the bug tracker or on the mailing list,
+* and you need other people's help to understand what's going wrong, please
+* include a debug log of the failing request with $c->dbg["ALL"] set, possibly
+* also using $c->dbg_filter on busy servers.
 */
 
 /**
@@ -13,17 +16,19 @@
 
 
 /**
-* If you want to debug you have to set to 1 one of this variable
-* and then you can look at the error log of PHP for example :
-* $c->dbg["ALL"] = 1;
-* and then tail -f /var/log/apache2/error_log (or wherever PHP errors are logged).
+* If you want to see debug messages in the PHP error log you can set to 1 one
+* or several of these variables (there are many more).
+* If you want to see every possible log message then $c->dbg["ALL"] can be set,
+* and then individual components can be set to 0 to selectively ignore them.
 *
-* in the code a line like this:
-* dbg_error_log( "Login", "blabla %s blalba %s",first_string, second_string );
-* will produce in apache error log and if $c->dbg['Login'] ==1 : "blabla first_string blalba second_string"
-* using format rules as for printf and related functions.
+* To follow the log, try
+*   tail -f /var/log/apache2/error_log   (or wherever your PHP errors are logged)
+*
+* To add your own debug logging using a printf syntax, use
+*   dbg_error_log( 'MyComponent', 'At this point, first is %s and second is %s', $first_string, $second_string );
 */
 // $c->dbg["ALL"] = 1;
+// $c->dbg['i18n'] = 0;
 // $c->dbg["request"] = 1;   // The request headers & content
 // $c->dbg['response'] = 1;  // The response headers & content
 // $c->dbg["component"] = 1;
@@ -42,6 +47,19 @@
 // $c->dbg['user'] = 1;
 // $c->dbg['vevent'] = 1;
 // $c->dbg['rrule'] = 1;
+
+
+/**
+* Even on a moderately busy server, turning on debug logging for everyone can
+* produce a lot of output in a short time that makes it hard to find the
+* relevant lines. Debug filtering limits logging to certain IP addresses or
+* usernames. (config values are arrays)
+*/
+// $c->dbg_filter["remoteIP"][] = '192.168.1.20';
+// $c->dbg_filter["remoteIP"][] = '192.168.1.21';
+// $c->dbg_filter["authenticatedUser"][] = 'peter';
+// $c->dbg_filter["authenticatedUser"][] = 'john';
+
 
 /**
 * default is 'davical' used to prefix debugging messages but will only need to change
