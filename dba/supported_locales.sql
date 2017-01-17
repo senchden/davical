senchden/davical

commit 504651a3f52cd138de27ea68bfe20c2f21b6d586

Add new locales and restructure how they are handled.
We now build the dba/supported_locales.sql file from po/locale.values
files for each locale, and we are also fetching the translations from
transifex.net in order to build them.

This commit also adds support for some new locales, especially
Brazilian Portuguese and Mexican Spanish.

dba/supported_locales.sql
@@ -1,28 +1,35 @@
 TRUNCATE supported_locales;
-
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'en_EN', 'English', 'English' );
+    VALUES( 'en', 'English', 'English' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'de_DE', 'German',  'Deutsch' );
+    VALUES( 'de', 'German',  'Deutsch' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'es_ES', 'Spanish', 'Español' );
+  VALUES( 'es_AR', 'Spanish (Argentina)', 'Español (Argentina)' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'fr_FR', 'French',  'Français' );
+  VALUES( 'es_MX', 'Spanish (Mexico)', 'Español (Mexico)' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'ru_RU', 'Russian',  'Русский' );
+    VALUES( 'es_VE', 'Spanish (Venezuela)', 'Español (Venezuela)' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'nl_NL', 'Netherlands',  'Nederlands' );
+    VALUES( 'et', 'Estonian',  'Eesti' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'pl_PL', 'Polish',  'Polski' );
+    VALUES( 'fr', 'French',  'Français' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'hu_HU', 'Hungarian',  'Magyar' );
+    VALUES( 'hu', 'Hungarian',  'Magyar' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'ja_JP', 'Japanese',  '日本語' );
+    VALUES( 'it', 'Italian',  'Italiano' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'it_IT', 'Italian',  'Italiano' );
+    VALUES( 'ja', 'Japanese',  '日本語' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'sv_SE', 'Swedish',  'Svenska' );
+    VALUES( 'nb', 'Norwegian',  'Bokmål' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'et_EE', 'Estonian',  'Eesti' );
+    VALUES( 'nl', 'Netherlands',  'Nederlands' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'nb_NO', 'Norwegian',  'Bokmål' );
+    VALUES( 'pl', 'Polish',  'Polski' );
+INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
+    VALUES( 'pt_BR', 'Brazilian Portuguese',  'Brazilian Portuguese' );
+INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
+    VALUES( 'pt_PT', 'Portuguese',  'Portuguese' );
+INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
+    VALUES( 'ru', 'Russian',  'Русский' );
+INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
+    VALUES( 'sv', 'Swedish',  'Svenska' );

po/de.values
@@ -1,0 +1,1 @@
+    VALUES( 'de', 'German',  'Deutsch' );

po/et.values
@@ -1,0 +1,1 @@
+    VALUES( 'et', 'Estonian',  'Eesti' );

po/fr.values
@@ -1,0 +1,1 @@
+    VALUES( 'fr', 'French',  'Français' );

po/hu.values
@@ -1,0 +1,1 @@
+    VALUES( 'hu', 'Hungarian',  'Magyar' );

po/it.values
@@ -1,0 +1,1 @@
+    VALUES( 'it', 'Italian',  'Italiano' );

po/ja.values
@@ -1,0 +1,1 @@
+    VALUES( 'ja', 'Japanese',  '日本語' );

po/nb_NO.values
@@ -1,0 +1,1 @@
+    VALUES( 'nb', 'Norwegian',  'Bokmål' );

commit 2a8469433116f4fd38fc78d5e7cdaef81ac8b164

Unbreak locale selection in admin interface
http://php.net/manual/en/function.setlocale.php says "Different systems
have different naming schemes for locales." This seems to be true for
platforms (eg Win vs. Linux), but on Debian AFAIR locales were always at
least two-part (de_DE), never just one (de). This seems to have been
broken in 504651a3, likely by mistake.

Also, switch locale immediately, even before writing to the database.

BTW, what happens when system default locale is configured to e.g. fr_FR
and the user selects "English" - does that work?

dba/supported_locales.sql
@@ -4,7 +4,7 @@ INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
     VALUES( 'ar', 'Arabic',  'العَرَبِية' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'de', 'German',  'Deutsch' );
+    VALUES( 'de_DE', 'German',  'Deutsch' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
   VALUES( 'es_AR', 'Spanish (Argentina)', 'Español (Argentina)' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
@@ -14,32 +14,32 @@ INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
     VALUES( 'es_VE', 'Spanish (Venezuela)', 'Español (Venezuela)' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'et', 'Estonian',  'Eesti' );
+    VALUES( 'et_EE', 'Estonian',  'Eesti' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'fi', 'Finnish',  'Suomi' );
+    VALUES( 'fi_FI', 'Finnish',  'Suomi' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'fr', 'French',  'Français' );
+    VALUES( 'fr_FR', 'French',  'Français' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'hu', 'Hungarian',  'Magyar' );
+    VALUES( 'hu_HU', 'Hungarian',  'Magyar' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'it', 'Italian',  'Italiano' );
+    VALUES( 'it_IT', 'Italian',  'Italiano' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'ja', 'Japanese',  '日本語' );
+    VALUES( 'ja_JP', 'Japanese',  '日本語' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
     VALUES( 'ko_KR', 'Korean (Korea)',  '한국어' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'nb', 'Norwegian',  'Bokmål' );
+    VALUES( 'nb_NO', 'Norwegian',  'Bokmål' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'nl', 'Netherlands',  'Nederlands' );
+    VALUES( 'nl_NL', 'Netherlands',  'Nederlands' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'pl', 'Polish',  'Polski' );
+    VALUES( 'pl_PL', 'Polish',  'Polski' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
     VALUES( 'pt_BR', 'Brazilian Portuguese',  'Brazilian Portuguese' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
     VALUES( 'pt_PT', 'Portuguese',  'Portuguese' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'ru', 'Russian',  'Русский' );
+    VALUES( 'ru_RU', 'Russian',  'Русский' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
     VALUES( 'sk_SK', 'Slovak (Slovakia)',  'Slovenčina' );
 INSERT INTO supported_locales ( locale, locale_name_en, locale_name_locale )
-    VALUES( 'sv', 'Swedish',  'Svenska' );
+    VALUES( 'sv_SE', 'Swedish',  'Svenska' );

inc/ui/principal-edit.php
@@ -220,6 +220,9 @@ function principal_editor() {
     if ( isset($_POST['fullname']) && !isset($_POST['displayname']) ) {
       $_POST['displayname'] = $_POST['fullname'];
     }
+    if ( isset($_POST['locale']) ) {
+      awl_set_locale($_POST['locale']); // activate immediately
+    }
     if ( isset($_POST['default_privileges']) ) {
       $privilege_bitpos = array_flip($privilege_names);
       $priv_names = array_keys($_POST['default_privileges']);

po/de.values
@@ -1,1 +1,1 @@
-    VALUES( 'de', 'German',  'Deutsch' );
+    VALUES( 'de_DE', 'German',  'Deutsch' );

po/et.values
@@ -1,1 +1,1 @@
-    VALUES( 'et', 'Estonian',  'Eesti' );
+    VALUES( 'et_EE', 'Estonian',  'Eesti' );

po/fi.values
@@ -1,1 +1,1 @@
-    VALUES( 'fi', 'Finnish',  'Suomi' );
+    VALUES( 'fi_FI', 'Finnish',  'Suomi' );

po/fr.values
@@ -1,1 +1,1 @@
-    VALUES( 'fr', 'French',  'Français' );
+    VALUES( 'fr_FR', 'French',  'Français' );

po/hu.values
@@ -1,1 +1,1 @@
-    VALUES( 'hu', 'Hungarian',  'Magyar' );
+    VALUES( 'hu_HU', 'Hungarian',  'Magyar' );

po/it.values
@@ -1,1 +1,1 @@
-    VALUES( 'it', 'Italian',  'Italiano' );
+    VALUES( 'it_IT', 'Italian',  'Italiano' );

po/ja.values
@@ -1,1 +1,1 @@
-    VALUES( 'ja', 'Japanese',  '日本語' );
+    VALUES( 'ja_JP', 'Japanese',  '日本語' );

po/nb_NO.values
@@ -1,1 +1,1 @@
-    VALUES( 'nb', 'Norwegian',  'Bokmål' );
+    VALUES( 'nb_NO', 'Norwegian',  'Bokmål' );

po/nl.values
@@ -1,1 +1,1 @@
-    VALUES( 'nl', 'Netherlands',  'Nederlands' );
+    VALUES( 'nl_NL', 'Netherlands',  'Nederlands' );

po/pl.values
@@ -1,1 +1,1 @@
-    VALUES( 'pl', 'Polish',  'Polski' );
+    VALUES( 'pl_PL', 'Polish',  'Polski' );

po/ru.values
@@ -1,1 +1,1 @@
-    VALUES( 'ru', 'Russian',  'Русский' );
+    VALUES( 'ru_RU', 'Russian',  'Русский' );

po/sv.values
@@ -1,1 +1,1 @@
-    VALUES( 'sv', 'Swedish',  'Svenska' );
+    VALUES( 'sv_SE', 'Swedish',  'Svenska' );