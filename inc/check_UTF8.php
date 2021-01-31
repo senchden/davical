f376be164eacf51d7354c728094e697ee2336218
Jan Hicken <jan.hicken@posteo.de>
2021-01-31 11:46:32 +0100

    Use brackets instead of curly braces for string offset access
    Curly braces have been deprecated in PHP 7.4 and unsupported in PHP 8.0.

--- inc/check_UTF8.php
+++ inc/check_UTF8.php
@@ -67,7 +67,7 @@ function utf8ToUnicode(&$str)
 
   $len = strlen($str);
   for($i = 0; $i < $len; $i++) {
-    $in = ord($str{$i});
+    $in = ord($str[$i]);
     if (0 == $mState) {
       // When mState is zero we expect either a US-ASCII character or a
       // multi-octet sequence.
