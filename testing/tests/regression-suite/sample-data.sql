3b9a3835726481ec33c678aee020160064169dff
Andrew Ruthven <puck@catalyst.net.nz>
2021-02-26 00:05:53 +1300

    Update tests to work with new usr_emails table

--- testing/tests/regression-suite/sample-data.sql
+++ testing/tests/regression-suite/sample-data.sql
@@ -1,50 +1,85 @@
 -- Some sample data to prime the database...
 -- base-data.sql should be processed before this
 
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 2, TRUE, current_date, current_date, 'andrew', '**x', 'Andrew McMillan', 'andrew@catalyst.net.nz' );
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 2, TRUE, current_date, current_date, 'andrew', '**x', 'Andrew McMillan' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES (2, 'andrew@catalyst.net.nz' );
 INSERT INTO role_member (user_no, role_no) VALUES( 2, 1);
 
 
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 10, TRUE, current_date, current_date, 'user1', '**user1', 'User 1', 'user1@example.net' );
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 11, TRUE, current_date, current_date, 'user2', '**user2', 'User 2', 'user2@example.net' );
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 12, TRUE, current_date, current_date, 'user3', '**user3', 'User 3', 'user3@example.net' );
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 13, TRUE, current_date, current_date, 'user4', '**user4', 'User 4', 'user4@example.net' );
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 14, TRUE, current_date, current_date, 'user5', '**user5', 'User 5', 'user5@example.net' );
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 15, TRUE, current_date, current_date, 'User Six', '**user6', 'User 6', 'user6@example.net' );
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 10, TRUE, current_date, current_date, 'user1', '**user1', 'User 1');
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 10, 'user1@example.net' );
+INSERT INTO usr_emails ( user_no, email, main )
+    VALUES( 10, 'user1+second@example.net', 'f' );
 
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 20, TRUE, current_date, current_date, 'manager1', '**manager1', 'Manager 1', 'manager1@example.net' );
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 11, TRUE, current_date, current_date, 'user2', '**user2', 'User 2');
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 11, 'user2@example.net' );
 
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 30, TRUE, current_date, current_date, 'assistant1', '**assistant1', 'Assistant 1', 'assistant1@example.net' );
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 12, TRUE, current_date, current_date, 'user3', '**user3', 'User 3' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 12, 'user3@example.net' );
+
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 13, TRUE, current_date, current_date, 'user4', '**user4', 'User 4' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 13, 'user4@example.net' );
+
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 14, TRUE, current_date, current_date, 'user5', '**user5', 'User 5' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 14, 'user5@example.net' );
+
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 15, TRUE, current_date, current_date, 'User Six', '**user6', 'User 6' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 15, 'user6@example.net' );
+
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 20, TRUE, current_date, current_date, 'manager1', '**manager1', 'Manager 1' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 20, 'manager1@example.net' );
+
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 30, TRUE, current_date, current_date, 'assistant1', '**assistant1', 'Assistant 1' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 30, 'assistant1@example.net' );
 
 
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 100, TRUE, current_date, current_date, 'resource1', '*salt*unpossible', 'Resource 1', 'resource1@example.net' );
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 100, TRUE, current_date, current_date, 'resource1', '*salt*unpossible', 'Resource 1' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 100, 'resource1@example.net' );
 INSERT INTO role_member (user_no, role_no) VALUES( 100, 4);
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 101, TRUE, current_date, current_date, 'resource2', '*salt*unpossible', 'Resource 2', 'resource2@example.net' );
+
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 101, TRUE, current_date, current_date, 'resource2', '*salt*unpossible', 'Resource 2' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 101, 'resource2@example.net' );
 INSERT INTO role_member (user_no, role_no) VALUES( 101, 4);
 
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 200, TRUE, current_date, current_date, 'resmgr1', '*salt*unpossible', 'Resource Managers', 'resource-managers@example.net' );
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 200, TRUE, current_date, current_date, 'resmgr1', '*salt*unpossible', 'Resource Managers' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 200, 'resource-managers@example.net' );
 INSERT INTO role_member (user_no, role_no) VALUES( 200, 2);
 
-INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname, email )
-    VALUES( 300, TRUE, current_date, current_date, 'teamclient1', '*salt*unpossible', 'Team for Client1', 'team-client1@example.net' );
+INSERT INTO usr ( user_no, active, email_ok, updated, username, password, fullname )
+    VALUES( 300, TRUE, current_date, current_date, 'teamclient1', '*salt*unpossible', 'Team for Client1' );
+INSERT INTO usr_emails ( user_no, email )
+    VALUES ( 300, 'team-client1@example.net' );
 INSERT INTO role_member (user_no, role_no) VALUES( 300, 2);
 
 SELECT setval('usr_user_no_seq', 1000);
 SELECT setval('dav_id_seq', 1000);
 
 UPDATE usr SET joined = '2009-06-01', updated = '2009-06-02';
+
 
 INSERT INTO collection (user_no, parent_container, dav_name, dav_etag,
                  dav_displayname, is_calendar, created, modified,
